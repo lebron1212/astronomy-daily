3c0bf04076135f487167840de692f3d6a355873b
lebron1212 <lebron1212@users.noreply.github.com>
2025-04-05 18:36:20 +0000

    deploy: 4952c378b560b592efc0c86694a34a72e08418ad

--- tracker.js
+++ tracker.js
@@ -1,29 +1,32 @@
 const eventList = document.getElementById("event-list");
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(fetchAstronomyData, showError);
-  } else {
-    eventList.innerHTML = "<p>Geolocation not supported by this browser.</p>";
-  }
+function fetchLocationAndData() {
+  fetch("https://ipapi.co/json/")
+    .then(res => res.json())
+    .then(loc => {
+      fetchAstronomyData(loc.latitude, loc.longitude);
+    })
+    .catch(err => {
+      eventList.innerHTML = "<p>Unable to get location from IP.</p>";
+      console.error("IP Location Error:", err);
+    });
 }
 
-function fetchAstronomyData(position) {
-  const { latitude, longitude } = position.coords;
-
-  const headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Basic " + btoa("65d206cd-4621-4456-a44e-07dc8c0f1d18:1065a5af9ef6e3c801b229d47a0be2bf6663fc6f6fc8a8d02a671ac41380570bbaf033ccae5cbdb2e362b412dcc8736300a1250d9ff55fb709441dc0738035500d932c17fce74e92f060cdcdaa0a68bbbcc2910da6d658663c6b8b4275f486b7594be878b3b7f7ef85e8d4e5800dc932") // <- Replaced by workflow only at runtime
-  };
-
-  const url = `https://api.astronomyapi.com/api/v2/bodies/positions?latitude=${latitude}&longitude=${longitude}&from_date=today&to_date=today&elevation=0`;
-
-  fetch(url, { headers })
-    .then(response => response.json())
-    .then(data => renderCelestialEvents(data.data.table.rows))
+function fetchAstronomyData(latitude, longitude) {
+  fetch("/.netlify/functions/astro", {
+    method: "POST",
+    body: JSON.stringify({ latitude, longitude }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (!data || !data.data || !data.data.table) {
+        throw new Error("Invalid response from AstronomyAPI");
+      }
+      renderCelestialEvents(data.data.table.rows);
+    })
     .catch(err => {
-      eventList.innerHTML = "<p>Error loading astronomy data.</p>";
-      console.error(err);
+      console.error("Fetch failed:", err);
+      eventList.innerHTML = `<p>Error loading celestial data.</p>`;
     });
 }
 
@@ -45,8 +48,4 @@ function renderCelestialEvents(bodies) {
   });
 }
 
-function showError(error) {
-  eventList.innerHTML = `<p>Geolocation error: ${error.message}</p>`;
-}
-
-getLocation();
+fetchLocationAndData();
